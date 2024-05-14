b8dc7ea5cf7c96896b3c68cca997ac40f0d01b7b
RebecaRakotomalala <ranjarakoto0@gmail.com>
2024-05-14 14:48:14 +0300

    14/05/24

--- scriptFrw.sh
+++ scriptFrw.sh
@@ -8,11 +8,14 @@ cd "$DIR"
 
 # Compiler le fichier FrontController.java
 javac -d . FrontController.java
+javac -d . AnnotationController.java
 
 # Créer un fichier JAR en incluant le fichier compilé FrontController.class
 jar cf FrontController.jar mg
+jar cf AnnotationController.jar mg
 
 # Déplacer le fichier JAR créé dans le répertoire souhaité
 mv FrontController.jar "R:\S4\Mr.Naina\sprint\test\lib"
+mv AnnotationController.jar "R:\S4\Mr.Naina\sprint\test\lib"
 
 sleep 60

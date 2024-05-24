24248ef24a413d3e78efa4208eeeff621155d2a4
RebecaRakotomalala <ranjarakoto0@gmail.com>
2024-05-24 08:57:33 +0300

    mise a jour

--- FrontController.java
+++ FrontController.java
@@ -1,80 +1,140 @@
 package mg.itu.prom16;
+
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
-
-import org.reflections.Reflections;
+import java.util.Map;
 
 public class FrontController extends HttpServlet {
-    private boolean checked = false;
-    private List<String> listeControllers = new ArrayList<>();
+    private final Map<String, Mapping> urlMapping = new HashMap<>();
 
     @Override
-    public void init() throws ServletException {
-        super.init();
-        scanControllers();
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        scanControllers(config);
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
     private synchronized void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>FrontController</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>URL actuelle :</h1>");
-        out.println("<p>" + request.getRequestURL() + "</p>");
-
-        if (!checked) {
-            scanControllers();
-            checked = true;
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>FrontController</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1 style='color:blue'>URL actuelle :</h1>");
+            out.println("<p>" + request.getRequestURL() + "</p>");
+    
+            String path = request.getPathInfo();
+            if (path == null) {
+                path = "/";
+            } else if (!path.startsWith("/")) {
+                path = "/" + path;
+            }
+            
+            Map<String, List<Mapping>> methodMappings = new HashMap<>();
+    
+            for (Mapping mapping : urlMapping.values()) {
+                String mappingKey = mapping.getKey();
+                if (!mappingKey.startsWith("/")) {
+                    mappingKey = "/" + mappingKey;
+                }
+                if (mappingKey.equals(path)) {
+                    String methodName = mapping.getMethod().getName();
+                    methodMappings.putIfAbsent(methodName, new ArrayList<>());
+                    methodMappings.get(methodName).add(mapping);
+                }
+            }
+    
+            if (!methodMappings.isEmpty()) {
+                out.println("<h2>Liste des contrôleurs et leurs méthodes annotées :</h2>");
+                out.println("<p>URL: " + path + "</p>");
+                for (Map.Entry<String, List<Mapping>> entry : methodMappings.entrySet()) {
+                    String methodName = entry.getKey();
+                    List<Mapping> mappings = entry.getValue();
+                    for (Mapping mapping : mappings) {
+                        out.println("<p>Classe: " + mapping.getControllerClass().getName() + "</p>");
+                        out.println("<p>Méthode: " + methodName + "</p>");
+                        out.println("<hr>");
+                    }
+                }
+            } else {
+                out.println("<h2 style='color:red'>Aucun mapping trouvé pour l'URL : " + path + "</h2>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+    
+    
 
-        out.println("<h2>Liste des contrôleurs annotés avec @AnnotationController :</h2>");
-        for (String controller : listeControllers) {
-            out.println("<p>" + controller + "</p>");
+    private void scanControllers(ServletConfig config) {
+        String controllerPackage = config.getInitParameter("controller-package");
+        System.out.println("Scanning package: " + controllerPackage);
+
+        try {
+            String path = "WEB-INF/classes/" + controllerPackage.replace('.', '/');
+            File directory = new File(getServletContext().getRealPath(path));
+            if (directory.exists()) {
+                scanDirectory(directory, controllerPackage);
+            } else {
+                System.out.println("Directory does not exist: " + directory.getAbsolutePath());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        out.println("</body>");
-        out.println("</html>");
-        out.close();
     }
 
-    private void scanControllers() {
-        ServletConfig config = getServletConfig();
-        String controllerPackage = config.getInitParameter("controller-package");
+    private void scanDirectory(File directory, String packageName) {
+        System.out.println("Scanning directory: " + directory.getAbsolutePath());
     
-        // Utilisez Reflections pour scanner le package et récupérer les classes annotées avec @AnnotationController
-        Reflections reflections = new Reflections(controllerPackage);
-        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(AnnotationController.class);
-    
-        // Parcourez chaque classe annotée pour récupérer les noms des classes
-        for (Class<?> clazz : annotatedClasses) {
-            // Vérifiez si la classe est annotée avec @AnnotationController
-            if (clazz.isAnnotationPresent(AnnotationController.class)) {
-                // Si oui, ajoutez le nom de la classe à la liste des contrôleurs
-                listeControllers.add(clazz.getName());
+        for (File file : directory.listFiles()) {
+            if (file.isDirectory()) {
+                scanDirectory(file, packageName + "." + file.getName());
+            } else if (file.getName().endsWith(".class")) {
+                String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                try {
+                    Class<?> clazz = Class.forName(className);
+                    if (clazz.isAnnotationPresent(AnnotationController.class)) {
+                        for (Method method : clazz.getDeclaredMethods()) {
+                            if (method.isAnnotationPresent(AnnotationMethode.class)) {
+                                AnnotationMethode requestMapping = method.getAnnotation(AnnotationMethode.class);
+                                String urlKey = requestMapping.value();
+                                if (!urlKey.startsWith("/")) {
+                                    urlKey = "/" + urlKey;
+                                }
+                                urlMapping.put(urlKey, new Mapping(urlKey, clazz, method));
+                                System.out.println("Mapped URL: " + urlKey + " to " + clazz.getName() + "." + method.getName());
+                            }
+                        }
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
-    
+
 }
--- README.md
+++ README.md
@@ -1,14 +1,3 @@
 # framwork0
 
-creer une class .java et annote le (exemple: @AnnotationController("TestController"))
-metter la dans une package (exemple: package controller)
-declarer votre package dans web.xml en utilisant "init-param" 
-(exemple:<servlet>
-            <servlet-name>FrontController</servlet-name>
-            <servlet-class>mg.itu.prom16.FrontController</servlet-class>
-            <init-param>
-                <param-name>controller-package</param-name>
-                <param-value>controller</param-value>
-            </init-param>
-         </servlet>)
-faire une script pour l'envoyer dans webapps si le dossier n'y est pas encore 
+creer une class avec des methodes qui seront annote avec AnnotationMethode (ex: @AnnotationMethode("mycontroller1"))

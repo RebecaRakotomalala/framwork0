87883e87827c9ba8a82c5f8ad21b4a36a9510ef1
RebecaRakotomalala <ranjarakoto0@gmail.com>
2024-06-14 10:40:54 +0300

    sprint6_2502

--- Param.java
+++ Param.java
@@ -4,10 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
- 
-@Target(ElementType.TYPE)
+
 @Retention(RetentionPolicy.RUNTIME)
-public @interface AnnotationController {
-
+@Target(ElementType.PARAMETER)
+public @interface Param {
+    String value();
 }
-

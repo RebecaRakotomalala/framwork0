27f70f4d8b19d7dd065893f09897982303068344
RebecaRakotomalala <ranjarakoto0@gmail.com>
2024-07-04 18:17:19 +0300

    spriny8_2502

--- FrontController.java
+++ FrontController.java
@@ -1,38 +1,47 @@
 package mg.itu.prom16;
 
-import jakarta.servlet.RequestDispatcher;
+import mg.itu.prom16.AnnotationController;
+import mg.itu.prom16.GetAnnotation;
+import mg.itu.prom16.Post;
+import mg.itu.prom16.Param;
+import mg.itu.prom16.CustomSession;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.net.URLDecoder;
+
+import mg.itu.prom16.ModelView; 
+import mg.itu.prom16.RequestBody;
+import java.lang.reflect.Field;  
+import java.io.*;
+import java.lang.reflect.Executable;
+import java.lang.reflect.Method;
+import java.net.URLDecoder;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import jakarta.servlet.RequestDispatcher; 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-import java.net.URLDecoder;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class FrontController extends HttpServlet {
-    private List<String> controllers = new ArrayList<>();
+    private List<String> controller = new ArrayList<>();
     private String controllerPackage;
-    private boolean checked = false;
-    private HashMap<String, Mapping> routes = new HashMap<>();
-    private String error = "";
+    boolean checked = false;
+    HashMap<String, Mapping> lien = new HashMap<>();
+    String error = "";
 
     @Override
     public void init() throws ServletException {
         super.init();
         controllerPackage = getInitParameter("controller-package");
         try {
-            scanControllers();
+            this.scan();
         } catch (Exception e) {
             error = e.getMessage();
         }
@@ -46,60 +55,56 @@ public class FrontController extends HttpServlet {
 
         response.setContentType("text/html");
         if (!error.isEmpty()) {
-            out.println("<p>Error: " + error + "</p>");
-        } else if (!routes.containsKey(controllerSearched)) {
+            out.println(error);
+        } else if (!lien.containsKey(controllerSearched)) {
             out.println("<p>Méthode non trouvée.</p>");
         } else {
             try {
-                Mapping mapping = routes.get(controllerSearched);
+                Mapping mapping = lien.get(controllerSearched);
                 Class<?> clazz = Class.forName(mapping.getClassName());
-                Method method = findMethodForRequest(clazz, mapping.getMethodName(), request.getMethod());
+                Method method = null;
+
+                // Find the method that matches the request type (GET or POST)
+                for (Method m : clazz.getDeclaredMethods()) {
+                    if (m.getName().equals(mapping.getMethodeName())) {
+                        if (request.getMethod().equalsIgnoreCase("GET") && m.isAnnotationPresent(GetAnnotation.class)) {
+                            method = m;
+                            break;
+                        } else if (request.getMethod().equalsIgnoreCase("POST") && m.isAnnotationPresent(Post.class)) {
+                            method = m;
+                            break;
+                        }
+                    }
+                }
 
                 if (method == null) {
                     out.println("<p>Aucune méthode correspondante trouvée.</p>");
                     return;
                 }
 
+                // Inject parameters
                 Object[] parameters = getMethodParameters(method, request);
-                Object controllerInstance = clazz.getDeclaredConstructor().newInstance();
-                Object returnValue = method.invoke(controllerInstance, parameters);
+                
+                Object object = clazz.getDeclaredConstructor().newInstance();
+                Object returnValue = method.invoke(object, parameters);
 
-                handleReturnValue(returnValue, request, response, out);
+                if (returnValue instanceof String) {
+                    out.println("Méthode trouvée dans " + returnValue);
+                } else if (returnValue instanceof ModelView) {
+                    ModelView modelView = (ModelView) returnValue;
+                    for (Map.Entry<String, Object> entry : modelView.getData().entrySet()) {
+                        request.setAttribute(entry.getKey(), entry.getValue());
+                    }
+                    RequestDispatcher dispatcher = request.getRequestDispatcher(modelView.getUrl());
+                    dispatcher.forward(request, response);
+                } else {
+                    out.println("Type de données non reconnu");
+                }
             } catch (Exception e) {
                 e.printStackTrace();
-                out.println("<p>ETU002502 , Methode non annote.</p>");
             }
         }
         out.close();
-    }
-
-    private Method findMethodForRequest(Class<?> clazz, String methodName, String requestMethod) {
-        for (Method method : clazz.getDeclaredMethods()) {
-            if (method.getName().equals(methodName)) {
-                if (requestMethod.equalsIgnoreCase("GET") && method.isAnnotationPresent(GetAnnotation.class)) {
-                    return method;
-                } else if (requestMethod.equalsIgnoreCase("POST") && method.isAnnotationPresent(Post.class)) {
-                    return method;
-                }
-            }
-        }
-        return null;
-    }
-
-    private void handleReturnValue(Object returnValue, HttpServletRequest request, HttpServletResponse response, PrintWriter out)
-            throws ServletException, IOException {
-        if (returnValue instanceof String) {
-            out.println("Méthode trouvée dans " + returnValue);
-        } else if (returnValue instanceof ModelView) {
-            ModelView modelView = (ModelView) returnValue;
-            for (Map.Entry<String, Object> entry : modelView.getData().entrySet()) {
-                request.setAttribute(entry.getKey(), entry.getValue());
-            }
-            RequestDispatcher dispatcher = request.getRequestDispatcher(modelView.getUrl());
-            dispatcher.forward(request, response);
-        } else {
-            out.println("Type de données non reconnu");
-        }
     }
 
     @Override
@@ -114,11 +119,11 @@ public class FrontController extends HttpServlet {
         processRequest(request, response);
     }
 
-    public void scanControllers() throws Exception {
+    public void scan() throws Exception {
         try {
             String classesPath = getServletContext().getRealPath("/WEB-INF/classes");
             String decodedPath = URLDecoder.decode(classesPath, "UTF-8");
-            String packagePath = decodedPath + File.separator + controllerPackage.replace('.', File.separatorChar);
+            String packagePath = decodedPath + "\\" + controllerPackage.replace('.', '\\');
             File packageDirectory = new File(packagePath);
             if (!packageDirectory.exists() || !packageDirectory.isDirectory()) {
                 throw new Exception("Package n'existe pas");
@@ -129,25 +134,39 @@ public class FrontController extends HttpServlet {
                         String className = controllerPackage + '.'
                                 + classFile.getName().substring(0, classFile.getName().length() - 6);
                         try {
-                            Class<?> clazz = Class.forName(className);
-                            if (clazz.isAnnotationPresent(AnnotationController.class)) {
-                                controllers.add(clazz.getSimpleName());
+                            Class<?> classe = Class.forName(className);
+                            if (classe.isAnnotationPresent(AnnotationController.class)) {
+                                controller.add(classe.getSimpleName());
 
-                                Method[] methods = clazz.getDeclaredMethods();
-                                for (Method method : methods) {
-                                    if (method.isAnnotationPresent(GetAnnotation.class)) {
-                                        addRoute(method, className, method.getAnnotation(GetAnnotation.class).value());
-                                    } else if (method.isAnnotationPresent(Post.class)) {
-                                        addRoute(method, className, method.getAnnotation(Post.class).value());
+                                Method[] methodes = classe.getDeclaredMethods();
+
+                                for (Method methode : methodes) {
+                                    if (methode.isAnnotationPresent(GetAnnotation.class)) {
+                                        Mapping map = new Mapping(className, methode.getName());
+                                        String valeur = methode.getAnnotation(GetAnnotation.class).value();
+                                        if (lien.containsKey(valeur)) {
+                                            throw new Exception("double url" + valeur);
+                                        } else {
+                                            lien.put(valeur, map);
+                                        }
+                                    } else if (methode.isAnnotationPresent(Post.class)) {
+                                        Mapping map = new Mapping(className, methode.getName());
+                                        String valeur = methode.getAnnotation(Post.class).value();
+                                        if (lien.containsKey(valeur)) {
+                                            throw new Exception("double url" + valeur);
+                                        } else {
+                                            lien.put(valeur, map);
+                                        }
                                     }
                                 }
                             }
                         } catch (Exception e) {
                             throw e;
                         }
+
                     }
                 } else {
-                    throw new Exception("Le package est vide");
+                    throw new Exception("le package est vide");
                 }
             }
         } catch (Exception e) {
@@ -155,69 +174,52 @@ public class FrontController extends HttpServlet {
         }
     }
 
-    private void addRoute(Method method, String className, String url) throws Exception {
-        Mapping map = new Mapping(className, method.getName());
-        if (routes.containsKey(url)) {
-            throw new Exception("URL déjà mappée: " + url);
-        } else {
-            routes.put(url, map);
-        }
-    }
-
-    private Object[] getMethodParameters(Method method, HttpServletRequest request) throws Exception {
-        Parameter[] parameters = method.getParameters();
-        Object[] parameterValues = new Object[parameters.length];
-
-        Enumeration<String> params = request.getParameterNames();
-        Map<String, String> paramMap = new HashMap<>();
-        while (params.hasMoreElements()) {
-            String paramName = params.nextElement();
-            paramMap.put(paramName, request.getParameter(paramName));
-        }
-
-        for (int i = 0; i < parameters.length; i++) {
-            Parameter parameter = parameters[i];
-            if (parameter.getType().equals(CustomSession.class)) {
-                HttpSession httpSession = request.getSession();
-                CustomSession customSession = (CustomSession) httpSession.getAttribute("customSession");
-                if (customSession == null) {
-                    customSession = new CustomSession();
-                    httpSession.setAttribute("customSession", customSession);
-                }
-                parameterValues[i] = customSession;
-            } else if (parameter.isAnnotationPresent(RequestBody.class)) {
-                parameterValues[i] = createRequestBodyParameter(parameter, paramMap);
-            } else if (parameter.isAnnotationPresent(Param.class)) {
-                Param param = parameter.getAnnotation(Param.class);
-                parameterValues[i] = paramMap.get(param.value()); // Supposons que tous les paramètres sont des chaînes pour simplifier
-            } else {
-                throw new IllegalArgumentException("ETU002502 le paramètre " + parameter.getName() + " de la méthode " + method.getName() + " n'est pas annoté avec @RequestBody ou @GetParam");
-            }
-        }
-        return parameterValues;
-    }
-
-    private Object createRequestBodyParameter(Parameter parameter, Map<String, String> paramMap) throws Exception {
+    private Object createRequestBodyParameter(Parameter parameter, Map<String, String[]> paramMap) throws Exception {
         Class<?> paramType = parameter.getType();
         Object paramObject = paramType.getDeclaredConstructor().newInstance();
         for (Field field : paramType.getDeclaredFields()) {
             String paramName = field.getName();
             if (paramMap.containsKey(paramName)) {
+                String paramValue = paramMap.get(paramName)[0]; // Assuming single value for simplicity
                 field.setAccessible(true);
-                field.set(paramObject, paramMap.get(paramName)); // Vous pouvez ajouter une conversion de type ici si nécessaire
+                field.set(paramObject, paramValue);
             }
         }
         return paramObject;
     }
+    
+    private Object[] getMethodParameters(Method method, HttpServletRequest request) throws Exception {
+        Parameter[] parameters = method.getParameters();
+        Object[] parameterValues = new Object[parameters.length];
+    
+        HttpSession session = request.getSession();
+    
+        for (int i = 0; i < parameters.length; i++) {
+            Parameter parameter = parameters[i];
+            if (parameter.getType() == CustomSession.class) {
+                parameterValues[i] = new CustomSession(session);
+            } else if (parameter.isAnnotationPresent(RequestBody.class)) {
+                parameterValues[i] = createRequestBodyParameter(parameter, request.getParameterMap());
+            } else if (parameter.isAnnotationPresent(Param.class)) {
+                Param param = parameter.getAnnotation(Param.class);
+                parameterValues[i] = request.getParameter(param.value()); // Assuming all parameters are strings for simplicity
+            } else {
+                throw new IllegalArgumentException("Paramètre non supporté pour cette méthode");
+            }
+        }
+    
+        return parameterValues;
+    }
+    
 }
 
 class Mapping {
-    private String className;
-    private String methodName;
+    String className;
+    String methodeName;
 
-    public Mapping(String className, String methodName) {
+    public Mapping(String className, String methodeName) {
         this.className = className;
-        this.methodName = methodName;
+        this.methodeName = methodeName;
     }
 
     public String getClassName() {
@@ -228,11 +230,11 @@ class Mapping {
         this.className = className;
     }
 
-    public String getMethodName() {
-        return methodName;
+    public String getMethodeName() {
+        return methodeName;
     }
 
-    public void setMethodName(String methodName) {
-        this.methodName = methodName;
+    public void setMethodeName(String methodeName) {
+        this.methodeName = methodeName;
     }
 }
